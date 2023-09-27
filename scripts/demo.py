dd5c9e438b7e52f72f3bf3e40982ad784388ee5e
salomaestro <chris10an.salomonsen@gmail.com>
2023-09-28 00:18:21 +0200

    linter updates

--- scripts/demo.py
+++ scripts/demo.py
@@ -13,7 +13,7 @@ from hisup.utils.visualizer import show_polygons
 def parse_args():
     parser = argparse.ArgumentParser(description='Testing')
 
-    parser.add_argument("--dataset", 
+    parser.add_argument("--dataset",
                         required=False,
                         type=str,
                         choices=['crowdai', 'inria'],
@@ -28,6 +28,7 @@ def parse_args():
     args = parser.parse_args()
 
     return args
+
 
 def inference_no_patching(cfg, model, image, device):
     transform = build_transform(cfg)
@@ -47,6 +48,7 @@ def inference_no_patching(cfg, model, image, device):
     else:
         print('No building polygons.')
 
+
 def inference_with_patching(cfg, model, image, device):
     import scipy.ndimage
     from tqdm import tqdm
@@ -56,24 +58,22 @@ def inference_with_patching(cfg, model, image, device):
     from hisup.utils.visualizer import viz_inria
 
     transform = build_transform(cfg)
-    
+
     h_stride, w_stride = 400, 400
     h_crop, w_crop = cfg.DATASETS.ORIGIN.HEIGHT, cfg.DATASETS.ORIGIN.WIDTH
-    h_img, w_img, _ = image.shape
-    h_grids = max(h_img - h_crop + h_stride - 1, 0) // h_stride + 1
     w_grids = max(w_img - w_crop + w_stride - 1, 0) // w_stride + 1
     pred_whole_img = np.zeros([h_img, w_img], dtype=np.float32)
     count_mat = np.zeros([h_img, w_img])
     juncs_whole_img = []
-    
+
     patch_weight = np.ones((h_crop + 2, w_crop + 2))
-    patch_weight[0,:] = 0
-    patch_weight[-1,:] = 0
-    patch_weight[:,0] = 0
-    patch_weight[:,-1] = 0
-    
+    patch_weight[0, :] = 0
+    patch_weight[-1, :] = 0
+    patch_weight[:, 0] = 0
+    patch_weight[:, -1] = 0
+
     patch_weight = scipy.ndimage.distance_transform_edt(patch_weight)
-    patch_weight = patch_weight[1:-1,1:-1]
+    patch_weight = patch_weight[1:-1, 1:-1]
 
     for h_idx in tqdm(range(h_grids), desc='processing on image'):
         for w_idx in range(w_grids):
@@ -83,10 +83,11 @@ def inference_with_patching(cfg, model, image, device):
             x2 = min(x1 + w_crop, w_img)
             y1 = max(y2 - h_crop, 0)
             x1 = max(x2 - w_crop, 0)
-            
+
             crop_img = image[y1:y2, x1:x2, :]
-            crop_img_tensor = transform(crop_img.astype(float))[None].to(device)
-            
+            crop_img_tensor = transform(crop_img.astype(float))[
+                None].to(device)
+
             meta = {
                 'height': crop_img.shape[0],
                 'width': crop_img.shape[1],
@@ -103,8 +104,8 @@ def inference_with_patching(cfg, model, image, device):
             juncs_whole_img.extend(juncs_pred.tolist())
             mask_pred *= patch_weight
             pred_whole_img += np.pad(mask_pred,
-                                ((int(y1), int(pred_whole_img.shape[0] - y2)),
-                                (int(x1), int(pred_whole_img.shape[1] - x2))))
+                                     ((int(y1), int(pred_whole_img.shape[0] - y2)),
+                                      (int(x1), int(pred_whole_img.shape[1] - x2))))
             count_mat[y1:y2, x1:x2] += patch_weight
 
     juncs_whole_img = np.array(juncs_whole_img)
@@ -117,15 +118,15 @@ def inference_with_patching(cfg, model, image, device):
         y1, x1, y2, x2 = prop.bbox
         bbox = [x1, y1, x2, y2]
         select_juncs = juncs_in_bbox(bbox, juncs_whole_img, expand=8)
-        poly, juncs_sa, _, _, juncs_index = generate_polygon(prop, pred_whole_img, \
-                                                                    select_juncs, pid=0, test_inria=True)
+        poly, juncs_sa, _, _, juncs_index = generate_polygon(prop, pred_whole_img,
+                                                             select_juncs, pid=0, test_inria=True)
         if juncs_sa.shape[0] == 0:
             continue
-        
+
         if len(juncs_index) == 1:
             polygons.append(Polygon(poly))
         else:
-            poly_ = Polygon(poly[juncs_index[0]], \
+            poly_ = Polygon(poly[juncs_index[0]],
                             [poly[idx] for idx in juncs_index[1:]])
             polygons.append(poly_)
 
@@ -145,7 +146,7 @@ def test(cfg, args):
         img_mean.append(np.mean(pixels))
         img_std.append(np.std(pixels))
     cfg.DATASETS.IMAGE.PIXEL_MEAN = img_mean
-    cfg.DATASETS.IMAGE.PIXEL_STD  = img_std
+    cfg.DATASETS.IMAGE.PIXEL_STD = img_std
 
     patching = False
     if H > 512 or W > 512:
