9da6687869cc2d2fc1540a2d270be96b34180a4a
salomaestro <chris10an.salomonsen@gmail.com>
2023-09-27 13:44:50 +0200

    run verify

--- hisup-demo.sh
+++ hisup-demo.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-python3 scripts/demo.py --dataset crowdai --img /storage/datasets/
+# python scripts/train.py --config-file config-files/crowdai-small_hrnet48.yaml 
+# python scripts/demo.py --dataset crowdai --im
+python scripts/verify_data.py --dataset crowdai
