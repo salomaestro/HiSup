8934d6469e763b291855098c74d3f9f3d79c324c
salomaestro <chris10an.salomonsen@gmail.com>
2023-10-03 20:01:12 +0200

    init scripts does the same, one in python, other in shell

--- init.sh
+++ init.sh
@@ -5,20 +5,9 @@ conda env create --file /storage/experiments/hisup/environment.yaml
 
 echo "=========================== ENV CREATED ==========================="
 
-which conda
-
-# export CUDA_HOME=/opt/conda/envs/hisup/pkgs/cuda-toolkit
 export CUDA_HOME=/opt/conda/envs/hisup
 
-# Add hisup bin to PATH
-export PATH=/storage/experiments/hisup/hisup/bin:$PATH
-
-echo $PATH
-
-echo "TEST CUDA HOME: $CUDA_HOME"
-ls $CUDA_HOME
-
-echo "=========================== CONTINUING BUILD ==========================="
+echo "=========================== STARTING BUILD ==========================="
 
 # conda run -n hisup make ### Instead of make just copy its contents
 cd /storage/experiments/hisup/hisup/csrc/lib/afm_op; conda run -n hisup python setup.py build_ext --inplace; rm -rf build
