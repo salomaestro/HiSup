c88b3041da3ee1a68a201e3183c50cc970afe4ea
salomaestro <chris10an.salomonsen@gmail.com>
2023-10-08 23:49:16 +0200

    clean messy repo (unused files + renames)

--- hisup-demo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# python scripts/train.py --config-file config-files/crowdai-small_hrnet48.yaml 
-python scripts/demo.py --dataset crowdai --img 000000000027.jpg
-# python scripts/verify_data.py --dataset crowdai
