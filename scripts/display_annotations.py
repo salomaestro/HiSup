689fcd61e36c6334b32c3cad5f353b0330e70a3a
salomaestro <chris10an.salomonsen@gmail.com>
2023-11-13 14:49:33 +0100

    better annotation displaying on random images per epoch during testing

--- scripts/display_annotations.py
+++ scripts/display_annotations.py
@@ -1,37 +1,65 @@
-from pycocotools.coco import COCO
+import json
+import random
+from argparse import ArgumentParser
+from pathlib import Path
+
+import matplotlib.pyplot as plt
 import numpy as np
 import skimage.io as io
-import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon, Patch
 from matplotlib.collections import PatchCollection
-import random
-import json
-from pathlib import Path
-from argparse import ArgumentParser
+from matplotlib.patches import Patch, Polygon
+from pycocotools.coco import COCO
 
 PREDICTION_COLOR = [0, 1, 0]
 GROUND_TRUTH_COLOR = [1, 0, 1]
 
-def generate_annotations(annotations, colors, ax):
+
+def generate_annotations_gt(annotations):
     polygons = []
 
     for ann in annotations:
-        for seg in ann["segmentation"]:
-            poly = np.array(seg).reshape((len(seg)//2, 2))
-            polygons.append(Polygon(poly))
+        seg = ann["segmentation"][0]
+        poly = np.array(seg).reshape((len(seg) // 2, 2))
+        polygons.append(Polygon(poly))
 
-    p = PatchCollection(polygons, facecolors=colors, linewidths=0, alpha=0.3)
-    ax.add_collection(p)
-    p = PatchCollection(polygons, facecolors="none", edgecolors=colors, linewidths=2)
-    ax.add_collection(p)
+    return polygons
+
+
+def generate_annotations_dt(annotations, ax):
+    polygons = []
+
+    for ann in annotations:
+        seg = ann["segmentation"][0]
+        bbox = ann["bbox"]
+        score = ann["score"]
+
+        poly = np.array(seg).reshape((len(seg) // 2, 2))
+        polygons.append(Polygon(poly))
+
+        bbox_center = (bbox[0] + bbox[2] // 2, bbox[1] + bbox[3] // 2)
+        ax.text(
+            bbox_center[0],
+            bbox_center[1],
+            f"{score:.3f}",
+            color="white",
+            size=8,
+            # backgroundcolor="black",
+            ha="center",
+            va="center",
+        )
+
+    return polygons
+
 
 def display_image(image, ax):
     ax.set_axis_off()
     ax.imshow(image)
 
+
 def load_prediction(path):
     with open(path) as f:
         return json.load(f)
+
 
 def create_index(gt_annotations_path, pred_annotations_path):
     gt = COCO(gt_annotations_path)
@@ -40,35 +68,81 @@ def create_index(gt_annotations_path, pred_annotations_path):
 
     return gt, results
 
-def display_annotations(gt, results, image_dir, image_id):
-    if image_id is None:
-        image_id = random.choice(gt.getImgIds())
 
+def get_random_image_id(gt):
+    return random.choice(gt.getImgIds())
+
+
+def load_image(image_path):
+    return io.imread(image_path)
+
+
+def get_image_ann(gt, results, image_dir, image_id=None):
+    # Choose random image if image_id is not provided
+    if image_id is None:
+        image_id = get_random_image_id(gt)
+
+    # Convert image_dir to Path if it is not already
+    if not isinstance(image_dir, Path):
+        image_dir = Path(image_dir)
+
+    # Get image meta data
     image_meta = gt.loadImgs(image_id)[0]
     image_path = image_dir / image_meta["file_name"]
-    image = io.imread(image_path)
 
+    # Get ground truth and prediction annotations
     gt_annotations_ids = gt.getAnnIds(imgIds=image_meta["id"])
     gt_annotations = gt.loadAnns(gt_annotations_ids)
 
     pred_annotations_ids = results.getAnnIds(imgIds=image_meta["id"])
     pred_annotations = results.loadAnns(pred_annotations_ids)
 
+    return image_path, gt_annotations, pred_annotations
+
+
+def display_annotations(gt, results, image_dir, image_id):
+    if image_id is None:
+        image_id = random.choice(gt.getImgIds())
+    if not isinstance(image_dir, Path):
+        image_dir = Path(image_dir)
+
+    image_path, gt_annotations, pred_annotations = get_image_ann(
+        gt,
+        results,
+        image_dir,
+        image_id,
+    )
+
+    image = load_image(image_path)
+
     legend_elements = [
         Patch(facecolor=GROUND_TRUTH_COLOR, edgecolor="none", label="Ground Truth"),
-        Patch(facecolor=PREDICTION_COLOR, edgecolor="none", label="Prediction")
+        Patch(facecolor=PREDICTION_COLOR, edgecolor="none", label="Prediction"),
     ]
 
-    plt.style.use('dark_background')
+    # plt.style.use("dark_background")
     fig, ax = plt.subplots(1, 1)
     ax.legend(handles=legend_elements, loc="lower center", bbox_to_anchor=(0.5, -0.27))
-    ax.set_title(image_meta["file_name"])
+    ax.set_title(image_path.name)
     ax.set_axis_off()
     ax.imshow(image)
 
-    generate_annotations(gt_annotations, GROUND_TRUTH_COLOR, ax)
-    generate_annotations(pred_annotations, PREDICTION_COLOR, ax)
+    gt_polygons = generate_annotations_gt(gt_annotations)
+    dt_polygons = generate_annotations_dt(pred_annotations, ax)
+
+    p = PatchCollection(gt_polygons, facecolors=GROUND_TRUTH_COLOR, linewidths=0, alpha=0.3)
+    ax.add_collection(p)
+    p = PatchCollection(gt_polygons, facecolors="none", edgecolors=GROUND_TRUTH_COLOR, linewidths=2)
+    ax.add_collection(p)
+
+    p = PatchCollection(dt_polygons, facecolors=PREDICTION_COLOR, linewidths=0, alpha=0.3)
+    ax.add_collection(p)
+    p = PatchCollection(dt_polygons, facecolors="none", edgecolors=PREDICTION_COLOR, linewidths=2)
+    ax.add_collection(p)
+
     plt.tight_layout()
+    return fig, ax
+
 
 def main(gt_annotations_path, pred_annotations_path, image_dir, image_id, num_images):
     gt, results = create_index(gt_annotations_path, pred_annotations_path)
@@ -78,9 +152,10 @@ def main(gt_annotations_path, pred_annotations_path, image_dir, image_id, num_im
         plt.show()
         return
 
-    for i in range(num_images):
+    for _ in range(num_images):
         display_annotations(gt, results, image_dir, None)
         plt.show()
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -97,5 +172,5 @@ if __name__ == "__main__":
         args.pred_annotations_path,
         args.image_dir,
         args.image_id,
-        args.num_images
+        args.num_images,
     )
--- scripts/test.py
+++ scripts/test.py
@@ -1,75 +1,125 @@
-import sys
-import os
 import argparse
 import logging
+import os
+import sys
+from pathlib import Path
+
 import wandb
 
 sys.path.append("/storage/experiments/hisup")
 
+import torch
+
 from hisup.config import cfg
 from hisup.detector import BuildingDetector
-from hisup.utils.logger import setup_logger
 from hisup.utils.checkpoint import DetectronCheckpointer
+from hisup.utils.logger import setup_logger
+from scripts.display_annotations import create_index, display_annotations
 from tools.test_pipelines import TestPipeline
 
-import torch
-torch.multiprocessing.set_sharing_strategy('file_system')
+torch.multiprocessing.set_sharing_strategy("file_system")
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Testing')
+    parser = argparse.ArgumentParser(description="Testing")
+    subparser = parser.add_subparsers(dest="command")
 
-    parser.add_argument("--config-file",
-                        metavar="FILE",
-                        help="path to config file",
-                        type=str,
-                        default=None,
-                        )
+    parser.add_argument(
+        "--config-file",
+        metavar="FILE",
+        help="path to config file",
+        type=str,
+        default=None,
+    )
 
-    parser.add_argument("--eval-type",
-                        type=str,
-                        help="evalutation type for the test results",
-                        default="coco_iou",
-                        choices=["coco_iou",  "boundary_iou", "polis"]
-                        )
+    parser.add_argument(
+        "--eval-type",
+        type=str,
+        help="evalutation type for the test results",
+        default="coco_iou",
+        choices=["coco_iou", "boundary_iou", "polis", "all"],
+    )
 
-    parser.add_argument("opts",
-                        help="Modify config options using the command-line",
-                        default=None,
-                        nargs=argparse.REMAINDER
-                        )
+    parser.add_argument(
+        "opts",
+        help="Modify config options using the command-line",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
+
+    full_eval_parser = subparser.add_parser("epochs")
+    full_eval_parser.add_argument(
+        "-f",
+        "--files",
+        type=Path,
+        help="path to .pth files of model at different epochs",
+        nargs="+",
+    )
+    full_eval_parser.add_argument(
+        "-a",
+        "--all",
+        help="evaluate all models in the output directory",
+        action="store_true",
+    )
 
     args = parser.parse_args()
-    
+
     return args
 
 
-def test(cfg, args):
+def test(cfg, args, epoch_file=None, epoch=None):
     logger = logging.getLogger("testing")
     device = cfg.MODEL.DEVICE
     model = BuildingDetector(cfg, test=True)
     model = model.to(device)
 
-    wandb.init(
-        project="hisup",
-        group="test",
-        config={
-            "model": cfg.MODEL.NAME,
-            "dataset": cfg.DATASETS.TRAIN[0],
-            }
+    if args.config_file is not None:
+        checkpointer = DetectronCheckpointer(
+            cfg,
+            model,
+            save_dir=cfg.OUTPUT_DIR,
+            save_to_disk=True,
+            logger=logger,
         )
 
-    if args.config_file is not None:
-        checkpointer = DetectronCheckpointer(cfg,
-                                         model,
-                                         save_dir=cfg.OUTPUT_DIR,
-                                         save_to_disk=True,
-                                         logger=logger)
-        _ = checkpointer.load()        
+        if epoch_file is None:
+            _ = checkpointer.load()
+        else:
+            _ = checkpointer.load(str(epoch_file), use_latest=False)
+
         model = model.eval()
 
-    test_pipeline = TestPipeline(cfg, args.eval_type, wandb.log)
-    test_pipeline.test(model)
-    # test_pipeline.eval()
+    test_pipeline = TestPipeline(cfg, args.eval_type, wandb.log, epoch)
+    result = test_pipeline.test(model)
+
+    gt_file = result["gt_file"]
+    dt_file = result["dt_file"]
+    # dt_mask_file = result["dt_mask_file"]
+    eval = result["eval"]
+
+    coco_iou = eval["coco_iou"][0]
+    boundary_iou = eval["boundary_iou"][0]
+    # polis = eval["polis"]
+
+    gt, dt = create_index(gt_file, dt_file)
+
+    img_dir = "/storage/datasets/crowdai/val/images"
+    images = []
+    for _ in range(3):
+        fig, _ = display_annotations(gt, dt, img_dir, None)
+        images.append(wandb.Image(fig))
+
+    wandb.log(
+        {
+            "test": {
+                "epoch": epoch,  # TODO: does not always have epoch
+                "coco_iou": coco_iou,
+                "boundary_iou": boundary_iou,
+                # "polis": polis,
+                "images": images,
+            }
+        }
+    )
 
 
 if __name__ == "__main__":
@@ -77,19 +127,51 @@ if __name__ == "__main__":
     if args.config_file is not None:
         cfg.merge_from_file(args.config_file)
     else:
-        cfg.OUTPUT_DIR = 'outputs/default'
-        os.makedirs(cfg.OUTPUT_DIR,exist_ok=True)
-    
+        cfg.OUTPUT_DIR = "outputs/default"
+        os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+
     cfg.merge_from_list(args.opts)
     cfg.freeze()
-    
+
     output_dir = cfg.OUTPUT_DIR
-    logger = setup_logger('testing', output_dir)
+    logger = setup_logger("testing", output_dir)
     logger.info(args)
     if args.config_file is not None:
         logger.info("Loaded configuration file {}".format(args.config_file))
     else:
         logger.info("Loaded the default configuration for testing")
 
-    test(cfg, args)
+    wandb.init(
+        project="hisup-multi",
+        group="test",
+        config={
+            "model": cfg.MODEL.NAME,
+            "dataset": cfg.DATASETS.TRAIN[0],
+        },
+    )
 
+    if args.command == "epochs":
+        if args.all:
+            files = sorted(Path(cfg.OUTPUT_DIR).glob("*.pth"))
+        else:
+            files = sorted(args.files)
+
+        logger.info(
+            "Testing models at epochs: {}".format(
+                list(map(lambda x: int(x.stem.replace("model_", "")), files))
+            )
+        )
+
+        for epoch_file in files:
+            # Get the epoch number from the filename
+            current_epoch = int(epoch_file.stem.replace("model_", ""))
+            logger.info("Testing model at epoch {}".format(current_epoch))
+
+            wandb.log({"test": {"epoch": current_epoch}})
+
+            test(cfg, args, epoch_file.absolute(), current_epoch)
+
+            logger.info("Finished testing model at epoch {}".format(current_epoch))
+
+    else:
+        test(cfg, args)
