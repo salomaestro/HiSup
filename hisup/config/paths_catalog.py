581b19ee0f4694e8dfbc96aa97371c12f7c5d66f
salomaestro <chris10an.salomonsen@gmail.com>
2024-02-20 13:56:56 +0100

    add new dataset

--- hisup/config/paths_catalog.py
+++ hisup/config/paths_catalog.py
@@ -3,36 +3,47 @@ import os.path as osp
 
 
 class DatasetCatalog(object):
-
     # DATA_DIR = osp.abspath(osp.join(osp.dirname(__file__),
     #             '..','..','data'))
     DATA_DIR = osp.abspath("/storage/datasets")
 
     DATASETS = {
-        'crowdai_train_small': {
-            'img_dir': 'crowdai/train/images',
-            'ann_file': 'crowdai/train/annotation-small.json'
+        "crowdai_train_small": {
+            "img_dir": "crowdai/train/images",
+            "ann_file": "crowdai/train/annotation-small.json",
         },
-        'crowdai_test_small': {
-            'img_dir': 'crowdai/val/images',
-            'ann_file': 'crowdai/val/annotation-small.json'
+        "crowdai_test_small": {
+            "img_dir": "crowdai/val/images",
+            "ann_file": "crowdai/val/annotation-small.json",
         },
-        'crowdai_train': {
-            'img_dir': 'crowdai/train/images',
-            'ann_file': 'crowdai/train/annotation.json'
+        "crowdai_train": {
+            "img_dir": "crowdai/train/images",
+            "ann_file": "crowdai/train/annotation.json",
         },
-        'crowdai_test': {
-            'img_dir': 'crowdai/val/images',
-            'ann_file': 'crowdai/val/annotation.json'
+        "crowdai_test": {
+            "img_dir": "crowdai/val/images",
+            "ann_file": "crowdai/val/annotation.json",
         },
-        'inria_train': {
-            'img_dir': 'inria/train/images',
-            'ann_file': 'inria/train/annotation.json',
+        "inria_train": {
+            "img_dir": "inria/train/images",
+            "ann_file": "inria/train/annotation.json",
         },
-        'inria_test': {
-            'img_dir': 'coco-Aerial/val/images',
-            'ann_file': 'coco-Aerial/val/annotation.json',
-        }
+        "inria_test": {
+            "img_dir": "coco-Aerial/val/images",
+            "ann_file": "coco-Aerial/val/annotation.json",
+        },
+        "terratec_train_small": {
+            "img_dir": "terratec/subset-32-1-514-127-20_39o/train/images",
+            "ann_file": "terratec/subset-32-1-514-127-20_39o/train/annotation.json",
+        },
+        "terratec_test_small": {
+            "img_dir": "terratec/subset-32-1-514-127-20_39o/test/images",
+            "ann_file": "terratec/subset-32-1-514-127-20_39o/test/annotation.json",
+        },
+        "terratec_val_small": {
+            "img_dir": "terratec/subset-32-1-514-127-20_39o/val/images",
+            "ann_file": "terratec/subset-32-1-514-127-20_39o/val/annotation.json",
+        },
     }
 
     @staticmethod
@@ -42,13 +53,12 @@ class DatasetCatalog(object):
         attrs = DatasetCatalog.DATASETS[name]
 
         args = dict(
-            root=osp.join(data_dir, attrs['img_dir']),
-            ann_file=osp.join(data_dir, attrs['ann_file'])
+            root=osp.join(data_dir, attrs["img_dir"]),
+            ann_file=osp.join(data_dir, attrs["ann_file"]),
         )
 
-        if 'train' in name:
+        if "train" in name:
             return dict(factory="TrainDataset", args=args)
-        if 'test' in name and 'ann_file' in attrs:
-            return dict(factory="TestDatasetWithAnnotations",
-                        args=args)
+        if "test" in name and "ann_file" in attrs:
+            return dict(factory="TestDatasetWithAnnotations", args=args)
         raise NotImplementedError()
