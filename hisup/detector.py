fe6c3f4587c4a11fee36644c8fcc14610a0253fe
salomaestro <chris10an.salomonsen@gmail.com>
2024-03-06 17:10:18 +0100

    BuildingDetectors forward test method now outputs afm, juncs and masks preceding the final prediction

--- hisup/detector.py
+++ hisup/detector.py
@@ -1,33 +1,35 @@
+from math import log
+
 import cv2
 import torch
 import torch.nn.functional as F
-
-from math import log
-from torch import nn
-from hisup.backbones import build_backbone
-from hisup.utils.polygon import generate_polygon
-from hisup.utils.polygon import get_pred_junctions
 from skimage.measure import label, regionprops
+from torch import nn
+
+from hisup.backbones import build_backbone
+from hisup.utils.polygon import generate_polygon, get_pred_junctions
 
 
 def cross_entropy_loss_for_junction(logits, positive):
     nlogp = -F.log_softmax(logits, dim=1)
 
-    loss = (positive * nlogp[:, None, 1] + (1 - positive) * nlogp[:, None, 0])
+    loss = positive * nlogp[:, None, 1] + (1 - positive) * nlogp[:, None, 0]
 
     return loss.mean()
 
-def sigmoid_l1_loss(logits, targets, offset = 0.0, mask=None):
+
+def sigmoid_l1_loss(logits, targets, offset=0.0, mask=None):
     logp = torch.sigmoid(logits) + offset
-    loss = torch.abs(logp-targets)
+    loss = torch.abs(logp - targets)
 
     if mask is not None:
         t = ((mask == 1) | (mask == 2)).float()
-        w = t.mean(3, True).mean(2,True)
-        w[w==0] = 1
-        loss = loss*(t/w)
+        w = t.mean(3, True).mean(2, True)
+        w[w == 0] = 1
+        loss = loss * (t / w)
 
     return loss.mean()
+
 
 # Copyright (c) 2019 BangguWu, Qilong Wang
 # Modified by Bowen Xu, Jiakun Xu, Nan Xue and Gui-song Xia
@@ -40,19 +42,19 @@ class ECA(nn.Module):
         k = t if t % 2 else t + 1
 
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.conv = nn.Conv1d(1, 1, kernel_size=k, padding=int(k/2), bias=False)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k, padding=int(k / 2), bias=False)
         self.sigmoid = nn.Sigmoid()
 
         self.out_conv = nn.Sequential(
             nn.Conv2d(channel, channel, kernel_size=1, padding=0, bias=False),
             nn.BatchNorm2d(channel),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=True),
         )
 
     def forward(self, x1, x2):
         y = self.avg_pool(x1 + x2)
         y = self.conv(y.squeeze(-1).transpose(-1, -2))
-        y = y.transpose(-1 ,-2).unsqueeze(-1)
+        y = y.transpose(-1, -2).unsqueeze(-1)
         y = self.sigmoid(y)
 
         out = self.out_conv(x2 * y.expand_as(x2))
@@ -65,9 +67,10 @@ class BuildingDetector(nn.Module):
         self.backbone = build_backbone(cfg)
         self.backbone_name = cfg.MODEL.NAME
         self.junc_loss = nn.CrossEntropyLoss()
-        self.test_inria = 'inria' in cfg.DATASETS.TEST[0]
+        self.test_inria = "inria" in cfg.DATASETS.TEST[0]
         if not test:
             from hisup.encoder import Encoder
+
             self.encoder = Encoder(cfg)
 
         self.pred_height = cfg.DATASETS.TARGET.HEIGHT
@@ -87,18 +90,18 @@ class BuildingDetector(nn.Module):
         self.jloc_predictor = self._make_predictor(dim_in, 3)
         self.afm_predictor = self._make_predictor(dim_in, 2)
 
-        self.refuse_conv = self._make_conv(2, dim_in//2, dim_in)
-        self.final_conv = self._make_conv(dim_in*2, dim_in, 2)
+        self.refuse_conv = self._make_conv(2, dim_in // 2, dim_in)
+        self.final_conv = self._make_conv(dim_in * 2, dim_in, 2)
 
         self.train_step = 0
-        
-    def forward(self, images, annotations = None):
+
+    def forward(self, images, annotations=None):
         if self.training:
             return self.forward_train(images, annotations=annotations)
         else:
             return self.forward_test(images, annotations=annotations)
 
-    def forward_test(self, images, annotations = None):
+    def forward_test(self, images, annotations=None):
         device = images.device
         outputs, features = self.backbone(images)
 
@@ -117,11 +120,11 @@ class BuildingDetector(nn.Module):
         remask_pred = self.final_conv(torch.cat((features, afm_conv), dim=1))
 
         joff_pred = outputs[:, :].sigmoid() - 0.5
-        mask_pred = mask_pred.softmax(1)[:,1:]
+        mask_pred = mask_pred.softmax(1)[:, 1:]
         jloc_convex_pred = jloc_pred.softmax(1)[:, 2:3]
         jloc_concave_pred = jloc_pred.softmax(1)[:, 1:2]
         remask_pred = remask_pred.softmax(1)[:, 1:]
-        
+
         scale_y = self.origin_height / self.pred_height
         scale_x = self.origin_width / self.pred_width
 
@@ -130,18 +133,25 @@ class BuildingDetector(nn.Module):
         batch_scores = []
         batch_juncs = []
 
+        extra_info = []
+
         for b in range(remask_pred.size(0)):
-            mask_pred_per_im = cv2.resize(remask_pred[b][0].cpu().numpy(), (self.origin_width, self.origin_height))
-            juncs_pred = get_pred_junctions(jloc_concave_pred[b], jloc_convex_pred[b], joff_pred[b])
-            juncs_pred[:,0] *= scale_x
-            juncs_pred[:,1] *= scale_y
+            mask_pred_per_im = cv2.resize(
+                remask_pred[b][0].cpu().numpy(), (self.origin_width, self.origin_height)
+            )
+            juncs_pred = get_pred_junctions(
+                jloc_concave_pred[b], jloc_convex_pred[b], joff_pred[b]
+            )
+            juncs_pred[:, 0] *= scale_x
+            juncs_pred[:, 1] *= scale_y
 
             if not self.test_inria:
                 polys, scores = [], []
                 props = regionprops(label(mask_pred_per_im > 0.5))
                 for prop in props:
-                    poly, juncs_sa, edges_sa, score, juncs_index = generate_polygon(prop, mask_pred_per_im, \
-                                                                            juncs_pred, 0, self.test_inria)
+                    poly, juncs_sa, edges_sa, score, juncs_index = generate_polygon(
+                        prop, mask_pred_per_im, juncs_pred, 0, self.test_inria
+                    )
                     if juncs_sa.shape[0] == 0:
                         continue
 
@@ -149,20 +159,36 @@ class BuildingDetector(nn.Module):
                     scores.append(score)
                 batch_scores.append(scores)
                 batch_polygons.append(polys)
-            
+
             batch_masks.append(mask_pred_per_im)
             batch_juncs.append(juncs_pred)
 
-        extra_info = {}
+            extra_info.append(
+                dict(
+                    masks=dict(
+                        afm_pred1=afm_pred[b, 0].cpu().numpy(),
+                        afm_pred2=afm_pred[b, 1].cpu().numpy(),
+                        mask_pred=mask_pred[b].cpu().numpy().squeeze(),
+                        remask_pred=remask_pred[b].cpu().numpy().squeeze(),
+                    ),
+                    juncs=dict(
+                        jloc_convex_pred=jloc_convex_pred[b].cpu().numpy().squeeze(),
+                        jloc_concave_pred=jloc_concave_pred[b].cpu().numpy().squeeze(),
+                        joff_pred1=joff_pred[b, 0].cpu().numpy(),
+                        joff_pred2=joff_pred[b, 1].cpu().numpy(),
+                    ),
+                )
+            )
+
         output = {
-            'polys_pred': batch_polygons,
-            'mask_pred': batch_masks,
-            'scores': batch_scores,
-            'juncs_pred': batch_juncs
+            "polys_pred": batch_polygons,
+            "mask_pred": batch_masks,
+            "scores": batch_scores,
+            "juncs_pred": batch_juncs,
         }
         return output, extra_info
 
-    def forward_train(self, images, annotations = None):
+    def forward_train(self, images, annotations=None):
         self.train_step += 1
 
         device = images.device
@@ -170,11 +196,11 @@ class BuildingDetector(nn.Module):
         outputs, features = self.backbone(images)
 
         loss_dict = {
-            'loss_jloc': 0.0,
-            'loss_joff': 0.0,
-            'loss_mask': 0.0,
-            'loss_afm' : 0.0,
-            'loss_remask': 0.0
+            "loss_jloc": 0.0,
+            "loss_joff": 0.0,
+            "loss_mask": 0.0,
+            "loss_afm": 0.0,
+            "loss_remask": 0.0,
         }
 
         mask_feature = self.mask_head(features)
@@ -192,15 +218,23 @@ class BuildingDetector(nn.Module):
         remask_pred = self.final_conv(torch.cat((features, afm_conv), dim=1))
 
         if targets is not None:
-            loss_dict['loss_jloc'] += self.junc_loss(jloc_pred, targets['jloc'].squeeze(dim=1))
-            loss_dict['loss_joff'] += sigmoid_l1_loss(outputs[:, :], targets['joff'], -0.5, targets['jloc'])
-            loss_dict['loss_mask'] += F.cross_entropy(mask_pred, targets['mask'].squeeze(dim=1).long())
-            loss_dict['loss_afm'] += F.l1_loss(afm_pred, targets['afmap'])
-            loss_dict['loss_remask'] += F.cross_entropy(remask_pred, targets['mask'].squeeze(dim=1).long())
+            loss_dict["loss_jloc"] += self.junc_loss(
+                jloc_pred, targets["jloc"].squeeze(dim=1)
+            )
+            loss_dict["loss_joff"] += sigmoid_l1_loss(
+                outputs[:, :], targets["joff"], -0.5, targets["jloc"]
+            )
+            loss_dict["loss_mask"] += F.cross_entropy(
+                mask_pred, targets["mask"].squeeze(dim=1).long()
+            )
+            loss_dict["loss_afm"] += F.l1_loss(afm_pred, targets["afmap"])
+            loss_dict["loss_remask"] += F.cross_entropy(
+                remask_pred, targets["mask"].squeeze(dim=1).long()
+            )
         extra_info = {}
 
         return loss_dict, extra_info
-    
+
     def _make_conv(self, dim_in, dim_hid, dim_out):
         layer = nn.Sequential(
             nn.Conv2d(dim_in, dim_hid, kernel_size=3, padding=1),
@@ -218,24 +252,28 @@ class BuildingDetector(nn.Module):
     def _make_predictor(self, dim_in, dim_out):
         m = int(dim_in / 4)
         layer = nn.Sequential(
-                    nn.Conv2d(dim_in, m, kernel_size=3, padding=1),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(m, dim_out, kernel_size=1),
-                )
+            nn.Conv2d(dim_in, m, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(m, dim_out, kernel_size=1),
+        )
         return layer
 
 
 def get_pretrained_model(cfg, dataset, device, pretrained=True):
     PRETRAINED = {
-        'crowdai': 'https://github.com/XJKunnn/pretrained_model/releases/download/pretrained_model/crowdai_hrnet48_e100.pth',
-        'inria': 'https://github.com/XJKunnn/pretrained_model/releases/download/pretrained_model/inria_hrnet48_e5.pth'
+        "crowdai": "https://github.com/XJKunnn/pretrained_model/releases/download/pretrained_model/crowdai_hrnet48_e100.pth",
+        "inria": "https://github.com/XJKunnn/pretrained_model/releases/download/pretrained_model/inria_hrnet48_e5.pth",
     }
 
     model = BuildingDetector(cfg, test=True)
     if pretrained:
         url = PRETRAINED[dataset]
-        state_dict = torch.hub.load_state_dict_from_url(url, map_location=device, progress=True)
-        state_dict = {k[7:]:v for k,v in state_dict['model'].items() if k[0:7] == 'module.'}
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, map_location=device, progress=True
+        )
+        state_dict = {
+            k[7:]: v for k, v in state_dict["model"].items() if k[0:7] == "module."
+        }
         model.load_state_dict(state_dict)
         model = model.eval()
         return model
